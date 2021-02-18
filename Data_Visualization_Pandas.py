822b55a071f5f6008846328272d3777c637dc21b
Olga Lazarenko <olazarenko@outlook.com>
2021-02-17 23:52:33 -0500

    read specific columns from the data file

--- Data_Visualization_Pandas.py
+++ Data_Visualization_Pandas.py
@@ -7,7 +7,20 @@ import pandas as pd
 # work with the data file 'Adults.csv' from the folder 'Adults_Data_Set'
 # read the file into the data frame 'adults_df'
 
-adults_df = pd.read_csv("E:\_Python_Projects_Data\Data_Visualization\Adults_Data_Set\Adults.csv")
+adults_df = pd.read_csv("E:\_Python_Projects_Data\Data_Visualization\Adults_Data_Set\Adults.csv" ,
+                        usecols  = ['Age' ,
+                                  'Work Class' ,
+                                  'Education' , 
+                                  'Marital Status' ,
+                                  'Occupation' , 
+                                  'Relationship' ,
+                                  'Race' ,
+                                  'Sex' , 
+                                  'Hours per Week' , 
+                                  'Country']
+                        )
 print(adults_df)
 print()
 print(adults_df.head())
+
+#
