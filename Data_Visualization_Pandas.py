05f202b17437c886f95d2a53486b138571aa0bd0
Olga Lazarenko <olazarenko@outlook.com>
2021-02-18 00:01:58 -0500

    show the scatter plot

--- Data_Visualization_Pandas.py
+++ Data_Visualization_Pandas.py
@@ -3,6 +3,7 @@
 
 # import Pandas library
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # work with the data file 'Adults.csv' from the folder 'Adults_Data_Set'
 # read the file into the data frame 'adults_df'
@@ -23,4 +24,9 @@ print(adults_df)
 print()
 print(adults_df.head())
 
-#
+# 1) ----  Scatter Plot  ----
+
+# scatter plot: 'Age' vs 'Hours per Week'
+adults_df.plot.scatter( x = 'Age' , y = 'Hours per Week' , title = 'Work hours and age')
+# to see the plot, add the Matplot syntax to show the plot
+plt.show()
