df6095b8c4041749bb60724c85d166714ba3c57d
Olga Lazarenko <olazarenko@outlook.com>
2021-02-19 23:17:52 -0500

    read a new file and create a new dataframe

--- Data_Visualization_Pandas.py
+++ Data_Visualization_Pandas.py
@@ -30,3 +30,25 @@ print(adults_df.head())
 adults_df.plot.scatter( x = 'Age' , y = 'Hours per Week' , title = 'Work hours and age')
 # to see the plot, add the Matplot syntax to show the plot
 plt.show()
+
+# 2) ----  Line Chart  ----
+# read the file 'Chargeable Income.csv'
+income_df = pd.read_csv("E:\_Python_Projects_Data\Data_Visualization\Company_Income\Chargeable_Income.csv")
+# show the first 5 rows of the data frame
+print('Chargeable Income data set')
+print(income_df.head())
+
+
+# 3) ----  Histogram  ----
+# plot the occurences of the hours per week
+adults_df['Hours per Week'].plot.hist()
+plt.show()
+
+# plot the frequencies of the age
+adults_df['Age'].plot.hist()
+plt.show()
+
+# multiple histograms
+
+
+
